9503c7044f18a5eedae2e97914c3860564d8136e
amAdeel <adeeleducationpurpose@gmail.com>
2024-10-31 10:19:40 +0500

    5th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Home from '@/Components/Home'
+import Homepractise from '@/Components/Homepractise'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+    <Homepractise/>
+    </>
   )
 }
 
