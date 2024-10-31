aff7287afbe85a3f1dc64b84c293afdcfd809763
amAdeel <adeeleducationpurpose@gmail.com>
2024-10-31 10:08:34 +0500

    4th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home() {
+import Home from '@/Components/Home'
+import React from 'react'
+
+const page = () => {
   return (
-    <></>
-  );
+    <div>
+      <Home/>
+    </div>
+  )
 }
+
+export default page
